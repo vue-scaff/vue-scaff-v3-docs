1be859d450ad37a6c87d82e55f2373c7420d9d16
joenix <joenix.com@gmail.com>
2022-01-17 20:49:28 +0800

    feat: update readme

--- docs/.vuepress/dist/assets/plugin.html.3ab2a376.js
+++
@@ -1,1 +0,0 @@
-const t={key:"v-5b6238ab",path:"/docs/plugin.html",title:"Plugins",lang:"zh-CN",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1641971893e3,contributors:[{name:"joenix",email:"joenix.com@gmail.com",commits:1}]},filePathRelative:"docs/plugin.md"};export{t as data};
---
+++ docs/.vuepress/dist/assets/plugin.html.ba6297b6.js
@@ -0,0 +1,1 @@
+const e={key:"v-5b6238ab",path:"/docs/plugin.html",title:"Plugins",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1641971893e3,contributors:[{name:"joenix",email:"joenix.com@gmail.com",commits:1}]},filePathRelative:"docs/plugin.md"};export{e as data};
